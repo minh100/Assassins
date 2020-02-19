5307e17acd0c385a87a2e8d1ecdfee91b0379db5
Daniel <danieloh@live.unc.edu>
2020-02-19 18:06:12 -0500

    daniel by himself

--- src/assassins/Main.java
+++ src/assassins/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main (String[] args) {
 			System.out.println("Yes I see this");
-			System.out.println("Daniel is here");
+			System.out.println("Daniel by himself");
 	}
 
 }
