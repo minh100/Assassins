d52c808e512b40aa423b29ad18f8f43393913ab3
Minh Truong <minh100@live.unc.edu>
2020-02-19 17:01:51 -0500

    fix

--- src/assassins/Main.java
+++ src/assassins/Main.java
@@ -2,7 +2,7 @@ package assassins;
 
 public class Main {
 
-	public void main (String[] args) {
+	public static void main (String[] args) {
 			System.out.println("Hello World");
 			System.out.println("Sup");
 			int j = 2;
