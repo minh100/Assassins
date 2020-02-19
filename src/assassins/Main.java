744c6a7bc44cfb59f6fb3b06d078c6156d461145
robgan <robgan503@gmail.com>
2020-02-19 17:27:51 -0500

    testing

--- src/assassins/Main.java
+++ src/assassins/Main.java
@@ -3,19 +3,7 @@ package assassins;
 public class Main {
 
 	public static void main (String[] args) {
-			System.out.println("Hello World");
-			System.out.println("Sup");
-			int j = 2;
-			j *= j;
-			System.out.println ( "test branches");
-			System.out.println ("testing merge");
-			System.out.println("Does this work in intellj?");
-			System.out.println("HELP");
-			System.out.println("DO you see this?");
+			System.out.println("Yes I see this");
 	}
-	// Testing
-	// testing plz
-	// intellij 2
-	// does this work at all?
 
 }
