d6c617515afdb4e7ea6db69aa464200c1faf485f
robgan <robgan503@gmail.com>
2020-02-21 16:23:36 -0500

    fizing merge conflicts?

--- src/assassins/Main.java
+++ src/assassins/Main.java
@@ -8,7 +8,7 @@ import java.util.Random;
 public class Main {
 
 	public static void main(String[] args) {
-
+		System.out.println("Yes I see this");
 		Scanner scan = new Scanner(System.in);
 
 		Random rand = new Random();
